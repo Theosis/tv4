8be7026ec7733ac93557179ad7c357932fba1c99
Bartvds <bartvanderschoor@gmail.com>
2013-12-10 20:05:48 +0100

    hardened stack logic

--- tv4.js
+++ tv4.js
@@ -1100,14 +1100,14 @@ function ValidationError(code, message, dataPath, schemaPath, subErrors) {
 	this.schemaPath = schemaPath || "";
 	this.subErrors = subErrors || null;
 
-	var err = new Error();
-	this.stack = (err.stack || err.stacktrace);
+	var err = new Error(this.message);
+	this.stack = err.stack || err.stacktrace;
 	if (!this.stack) {
 		try {
 			throw err;
 		}
 		catch(err) {
-			this.stack = err.stack;
+			this.stack = err.stack || err.stacktrace;
 		}
 	}
 }
